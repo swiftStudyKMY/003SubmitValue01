c08d73025e7bdb807352939bf65061dbb207c2eb
MinyoungKim <801sanae@gmail.com>
2019-11-11 23:53:36 +0900

    VC1 -> VC2  전달받은 데이터 화면에 표시

--- 003SubmitValue01/ResultViewController.swift
+++ 003SubmitValue01/ResultViewController.swift
@@ -23,9 +23,12 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         self.resultEmail.text = paramEmail
-        self.resultUpdate.text = (self.paramUpdate = true ? "자동갱신" : "자동갱신안함")
+        self.resultUpdate.text = (self.paramUpdate == true ? "자동갱신" : "자동갱신안함")
         self.resultInterval.text = "\(Int(paramInerval)) 분 마다 갱신"
         
     }
     
+    @IBAction func onBack(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
 }
